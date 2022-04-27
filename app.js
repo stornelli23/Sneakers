faeab1d9c3ec40aee3907642a273d06392f2b640
MatiasFeru <matias.feruglio@hotmail.com>
2022-04-26 21:01:50 -0300

    Agrege login html y css

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
 })
 
-app.get('', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/'))
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
 })
 
 app.get('', (req, res) => {
