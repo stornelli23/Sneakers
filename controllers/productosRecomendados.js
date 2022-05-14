5c64ba435366d5340e46e807c07ba07cdf9a58f1
Marina Diez <marinafdiez@gmail.com>
2022-05-14 13:59:09 -0300

    hicimos dinamicos algunos elementos

---
+++ controllers/productosRecomendados.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+// Productos recomendados//
+
+const productosRecomendados =[{
+    imagen: '/nike-air-huarache.jpg',
+    detalleDeProducto: 'Nike Aire Huarache',
+    precioProducto: '$25.000',
+},
+{
+    imagen: '/airforce.png',
+    detalleDeProducto: 'Nike Airforce',
+    precioProducto: '$10.000',
+},
+{
+    imagen: '/nike-air-huarache.jpg',
+    detalleDeProducto: 'Nike Aire Huarache',
+    precioProducto: '$25.000',
+}
+,
+{
+    imagen: '/airforce.png',
+    detalleDeProducto: 'Nike Airforce',
+    precioProducto: '$10.000',
+},
+]
+
+
+
+
+module.exports = productosRecomendados;
