a4572cb6667d5226c3569fa147a08369806bce06
stornelli23 <santystornelli@hotmail.com>
2022-06-24 19:40:00 -0300

    proceso de register

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,44 +1,66 @@
-const fs = require('fs');
-const path = require('path');
-const bcrypt = require('bcryptjs');
+const fs = require("fs");
+const path = require("path");
 
-const userFilePath = path.join(__dirname,'../data/users.json')
-const usersJson = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'))
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+const userFilePath = path.join(__dirname, "../data/users.json");
+const usersJson = JSON.parse(fs.readFileSync(userFilePath, "utf-8"));
 
 const usersControllers = {
-index: (req,res) => {
-    res.render('users', {usersJson});
-},
-login: (req, res) => {
-    res.render('login')
-},
+  index: (req, res) => {
+    res.render("users", { usersJson });
+  },
+  login: (req, res) => {
+    res.render("login");
+  },
 
-register: (req, res) => {
-    res.render('register')
-},
-saveUser: (req, res) => {
-    let image
-    if(req.files[0] != undefined){
-        image = req.files[0].filename
+  register: (req, res) => {
+    res.render("register");
+  },
+  processRegister: (req, res) => {
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
     } else {
-        image = 'default-image.jpeg'
+      res.render("register", { 
+        errors: errors.mapped(), 
+        old: req.body });
     }
 
+    let image 
+        if(req.files[0] != undefined){
+            image = req.files[0].filename
+        } else {
+            image = 'avatardefault.png'
+        };
+
     let newUser = {
-        
-        user_id: usersJson[usersJson.length-1].user_id+1,
+        user_id: User.generateId(),
         ...req.body,
         avatar: image,
         password: bcrypt.hashSync(req.body.password, 10)
     }
+    usersJson.push(newUser);
+    fs.writeFileSync(userFilePath, JSON.stringify(usersJson, null, ' '));
+    res.redirect("/users");
+  },
 
-    let comparacion = bcrypt.compareSync('$2a$10$NWpGN4joq7wd23RqbtT7yeZXh825kQLmZ4jFC/PmaOOA1IB4DtVCu', 'holasoyhola');
-    console.log(comparacion)
-    
-    usersJson.push(newUser) ;      
-    fs.writeFileSync(userFilePath, JSON.stringify(usersJson));
-    res.redirect('/users')
-    },
+  profile: (req, res) => {
+    res.render("userProfile");
+  },
+
+  loginProcess: (req, res) => {
+    let resultValidation = validationResult(req);
+
+    if (resultValidation.errors.isEmpty()) {
+    } else {
+      res.render("login", { 
+        errors: resultValidation.mapped(), 
+        old: req.body });
+    }
+  },
 };
 
 module.exports = usersControllers;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const multer  = require('multer');
 const path = require ('path');
 const imgFilePath = path.join(__dirname, '../public/img/users');
+const { check } = require('express-validator');
 
 ///*** Controller Require **** */
 const usersControllers = require('../controllers/usersControllers');
@@ -24,14 +25,45 @@ const storage = multer.diskStorage({
   
 const upload = multer({ storage: storage })
 
+/*VALIDACIONES*/
+
+const validateLogin = [
+  check('email-usuario')
+    .notEmpty()
+    .withMessage('Este campo no puede estar vacio').bail()
+    .isEmail().withMessage('Ingrese un email valido'),
+
+  check('password-usuario')
+    .notEmpty().withMessage('Este campo no puede estar vacio')
+]
+
+const validateRegister = [
+  check('first_name').notEmpty()
+  .withMessage('Este campo no puede estar vacio').bail(),
+
+  check('last_name').notEmpty()
+  .withMessage('Este campo no puede estar vacio').bail(),
+
+  check('email').notEmpty()
+  .withMessage('Este campo no puede estar vacio').bail().isEmail().withMessage('Ingrese un email valido'),
+
+  check('password').notEmpty()
+  .withMessage('Este campo no puede estar vacio').bail(),
+
+  check('date_of_birth'),
+  check('gender')
+]
+
 /*RUTAS POR GET*/
 
 router.get('/users', usersControllers.index);
 router.get('/login', usersControllers.login);
 router.get('/register', usersControllers.register);
+router.get('/userProfile/:id', usersControllers.profile);
 
 /*RUTAS POR POST*/
 
-router.post('/register', upload.any(), usersControllers.saveUser);
+router.post('/register', upload.any(), validateRegister, usersControllers.processRegister);
+router.post('/login', validateLogin, usersControllers.loginProcess);
 
 module.exports = router;
