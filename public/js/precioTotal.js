695b2fda3fed2f68dbb5a40cf345a725ce172dc2
nguetti <nicanorguetti@gmail.com>
2022-08-28 18:55:28 -0300

    continuo trabajando en subtotales carrito

--- public/js/precioTotal.js
+++ public/js/precioTotal.js
@@ -1,15 +1,20 @@
+
 // if(localStorage.precioTotal){
 //     document.querySelector("p.total-precio").innerText = localStorage.precioTotal
     
 // }
-//INCONCLUSO AL 28/08 SOLO LEVANTA EL DATO DEL PRECIO TOTAL DEL BACK
-let precioTotal = Number(document.querySelector("p.total-precio").innerText)
 
-    localStorage.setItem("precioTotal", precioTotal)
+
+
+//INCONCLUSO AL 28/08 SOLO LEVANTA EL DATO DEL PRECIO TOTAL DEL BACK
+// let precioTotal = Number(document.querySelector("p.total-precio").innerText)
+
+
+//     sessionStorage.setItem("precioTotal", precioTotal)
    
     
    
-    console.log(precioTotal)
+    
 
 
 
