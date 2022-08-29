ce5adaadf64fece089709e95953fc7f96c83473f
stornelli23 <santystornelli@hotmail.com>
2022-08-29 08:11:05 -0300

    cambios en api controllers

--- controllers/apiControllers/apiProductsController.js
+++ controllers/apiControllers/apiProductsController.js
@@ -1,14 +1,11 @@
 const db = require("../../database/models");
-const { productCart } = require("../productsController");
 
 const apiProductsController = {
   allProducts: (req, res) => {
-    db.Product.findAll({
-      attributes: ["id", "name", "description", "category_id"],
-    }).then((products) => {
-      let totalHombre = 0;
-      let totalMujer = 0;
-      let totalUnisex = 0;
+    db.Product.findAll().then((products) => {
+      let totalNike = 0;
+      let totalAdidas = 0;
+      let totalReebok = 0;
 
       for (let i = 0; i < products.length; i++) {
         products[i].setDataValue(
@@ -16,31 +13,32 @@ const apiProductsController = {
           "http://localhost:3000/productDetail/" + products[i].id
         );
 
-        if (products[i].category_id == 1) {
-          totalHombre++;
+        if (products[i].brand_id == 1) {
+          totalNike++;
         }
-        if (products[i].category_id == 2) {
-          totalMujer++;
+        if (products[i].brand_id == 2) {
+          totalAdidas++;
         }
-        if (products[i].category_id == 3) {
-          totalUnisex++;
+        if (products[i].brand_id == 3) {
+          totalReebok++;
         }
       }
 
       res.status(200).json({
-        total: products.length,
-        countByCategory: [
+        totalProducts: products.length,
+        totalBrands: totalNike + totalAdidas + totalReebok,
+        countByBrand: [
           {
-            Hombre: totalHombre,
+           Nike: totalNike,
           },
           {
-            Mujer: totalMujer,
+            Adidas: totalAdidas,
           },
           {
-            Unisex: totalUnisex,
+            Reebok: totalReebok,
           },
         ],
-        data: products,
+        products: products,
         status: 200,
       });
     });
@@ -49,13 +47,9 @@ const apiProductsController = {
   productDetail: async (req, res) => {
     let id = req.params.id;
 
-    await db.Product.findOne({where: "id" == id}, {
-      include: [{ 
-        model: Category,
-        as: 'categories'
-      }]
-    }).then((product) => {
-      
+    await db.Product.findByPk(id)
+    .then((product) => {
+      res.json(resultado)
 
       res.status(200).json({
         data: {
@@ -66,7 +60,7 @@ const apiProductsController = {
           description: product.description,
           image: "http://localhost:3000/img/products/" + product.image,
           productCategory: [
-            { Category: product.Category.category },
+            { Category: product.category_id },
             { Brand: product.brand_id },
           ],
         },
